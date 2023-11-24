d74a7dd432717eca68af52d67f1573b5187675c8
Alexander Eriksson <alex.store@hotmail.com>
2023-11-24 23:09:48 +0100

    add dist

---
+++ dist/apis/lol/challenges/challenges.d.ts
@@ -0,0 +1,37 @@
+import { Regions, Levels } from '../../../constants';
+import { ChallengeConfigV1DTO, ChallengeLeaderboardEntryV1DTO, PercentilesV1DTO, ProfileV1DTO } from '../../../models-dto/challenges/playerData.dto';
+import { BaseApiLol } from '../base/base.api.lol';
+/**
+ * Challenges methods
+ */
+export declare class ChallengesApi extends BaseApiLol {
+    /**
+     * Get config
+     */
+    getConfig(region: Regions): Promise<import("../../../models-dto").ApiResponseDTO<ChallengeConfigV1DTO[]>>;
+    /**
+     * Get percentiles
+     */
+    getPercentiles(region: Regions): Promise<import("../../../models-dto").ApiResponseDTO<PercentilesV1DTO[]>>;
+    /**
+     * Get getChallengePercentile by challengeId
+     * @param challengeId
+     */
+    getChallengeConfig(challengeId: number, region: Regions): Promise<import("../../../models-dto").ApiResponseDTO<ChallengeConfigV1DTO>>;
+    /**
+     * Get Leaderboards By Level
+     * @param challengeId
+     * @param level rank tier
+     */
+    getLeaderboardsByLevel(challengeId: number, level: Levels, region: Regions): Promise<import("../../../models-dto").ApiResponseDTO<ChallengeLeaderboardEntryV1DTO>>;
+    /**
+     * Get Challenge Percentiles
+     * @param challengeId
+     */
+    getChallengePercentiles(challengeId: number, region: Regions): Promise<import("../../../models-dto").ApiResponseDTO<PercentilesV1DTO>>;
+    /**
+     * Get player data
+     * @param puuid
+     */
+    getPlayerData(puuid: string, region: Regions): Promise<import("../../../models-dto").ApiResponseDTO<ProfileV1DTO>>;
+}
---
+++ dist/constants/level.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Levels = void 0;
+/**
+ * Levels
+ * Options from riot dev portal. Unclear which are acceptable. Options are the same as in the dropdown on the website.
+ */
+var Levels;
+(function (Levels) {
+    Levels["NONE"] = "NONE";
+    Levels["IRON"] = "IRON";
+    Levels["BRONZE"] = "BRONZE";
+    Levels["SILVER"] = "SILVER";
+    Levels["GOLD"] = "GOLD";
+    Levels["PLATINUM"] = "PLATINUM";
+    Levels["DIAMOND"] = "DIAMOND";
+    Levels["MASTER"] = "MASTER";
+    Levels["GRANDMASTER"] = "GRANDMASTER";
+    Levels["CHALLENGER"] = "CHALLENGER";
+    Levels["HIGHEST_NOT_LEADERBOARD_ONLY"] = "HIGHEST_NOT_LEADERBOARD_ONLY";
+    Levels["HIGHEST"] = "HIGHEST";
+    Levels["LOWEST"] = "LOWEST";
+})(Levels = exports.Levels || (exports.Levels = {}));
