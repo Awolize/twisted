1684ed6ba71c672bf9a3959ba7a74d15129a3e6d
Alexander Eriksson <alex.store@hotmail.com>
2023-11-25 18:28:10 +0100

    fixup

--- dist/apis/index.d.ts
+++ dist/apis/index.d.ts
@@ -1,2 +1,3 @@
 export * from './lol/lol';
 export * from './tft/tft';
+export * from './riot/riot';
--- dist/apis/index.js
+++ dist/apis/index.js
@@ -12,3 +12,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./lol/lol"), exports);
 __exportStar(require("./tft/tft"), exports);
+__exportStar(require("./riot/riot"), exports);
