43370f8e7bc8d7c98431a0796596001893cdca7c
Alexander Eriksson <alex.store@hotmail.com>
2023-12-28 18:49:47 +0100

    test

--- dist/apis/lol/champion/champion.d.ts
+++ dist/apis/lol/champion/champion.d.ts
@@ -6,41 +6,27 @@ import { BaseApiLol } from "../base/base.api.lol";
  * Third party methods
  */
 export declare class ChampionApi extends BaseApiLol {
-	/**
-	 * Get champion rotation
-	 * @param region Riot region
-	 */
-	rotation(
-		region: Regions,
-	): Promise<import("../../../models-dto").ApiResponseDTO<unknown>>;
-	/**
-	 * Champion mastery by puuid
-	 * @param encryptedPUUID
-	 * @param region
-	 */
-	masteryByPUUID(
-		encryptedPUUID: string,
-		region: Regions,
-	): Promise<
-		import("../../../models-dto").ApiResponseDTO<ChampionMasteryDTO[]>
-	>;
-	/**
-	 * Champion mastery by summoner
-	 * @param encryptedPUUID
-	 * @param region
-	 */
-	masteryByPUUIDChampion(
-		encryptedPUUID: string,
-		championId: number,
-		region: Regions,
-	): Promise<import("../../../models-dto").ApiResponseDTO<ChampionMasteryDTO>>;
-	/**
-	 * Champions mastery score
-	 * @param encryptedPUUID
-	 * @param region
-	 */
-	championsScore(
-		encryptedPUUID: string,
-		region: Regions,
-	): Promise<ChampionsScoreDTO>;
+    /**
+     * Get champion rotation
+     * @param region Riot region
+     */
+    rotation(region: Regions): Promise<import("../../../models-dto").ApiResponseDTO<unknown>>;
+    /**
+     * Champion mastery by summoner
+     * @param encryptedPUUID
+     * @param region
+     */
+    masteryByPUUID(encryptedPUUID: string, region: Regions): Promise<import("../../../models-dto").ApiResponseDTO<ChampionMasteryDTO[]>>;
+    /**
+     * Champion mastery by summoner
+     * @param encryptedPUUID
+     * @param region
+     */
+    masteryByPUUIDChampion(encryptedPUUID: string, championId: number, region: Regions): Promise<import("../../../models-dto").ApiResponseDTO<ChampionMasteryDTO>>;
+    /**
+     * Champions mastery score
+     * @param encryptedPUUID
+     * @param region
+     */
+    championsScore(encryptedPUUID: string, region: Regions): Promise<ChampionsScoreDTO>;
 }
