fd634a07565ae187759d6c7da54e661763d4c12b
Alexander Eriksson <alex.store@hotmail.com>
2023-12-28 10:25:23 +0100

    new build with cba35ae5344cbeac1e5d34fc7e6f5b93b03b8162, fixup, forgot to export challenges types

--- dist/models-dto/challenges/index.js
+++ dist/models-dto/challenges/index.js
@@ -15,3 +15,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./challenges.dto"), exports);
+__exportStar(require("./playerData.dto"), exports);
